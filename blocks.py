1e18f46a2fbb753cb8b28ecd2fd7b6a01b8e694b
Sixtus Miracle <67114923+SixtusMiracle@users.noreply.github.com>
2022-04-14 10:55:20 +0100

    playing with conditionals

--- blocks.py
+++ blocks.py
@@ -1,8 +1,19 @@
 m = 53
 n = 82
 
-if n > m:
-    z = 100
+if n < m:
+    print(f'n < m: n is {n} and m is {m}')
+elif n > m:
     print(f'n > m: n is {n} and m is {m}')
+elif n == m:
+    print(f'n == m: n is {n} and m is {m}')
+else:
+    print('something else')
 
-print(z)
+# no switch and case
+if m == 5:
+    print(f'm is {m}')
+if m == 6:
+    print(f'm is {m}')
+if m == 53:
+    print(f'm is {m}')
