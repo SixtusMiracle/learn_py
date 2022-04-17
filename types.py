ec2be55311669e8c5b2f669085361f419fc99435
Sixtus Miracle <67114923+SixtusMiracle@users.noreply.github.com>
2022-04-17 13:03:26 +0100

    comparing objects
    type, id and isinstance

--- types.py
+++ types.py
@@ -1,7 +1,28 @@
-#  None - NoneType is False
-# numeric 0 is False other numbers are True
-# empty string is '' is False
+x = (1, 'two', 3.0, [4, 'four'], 5)
+y = [1, 'two', 3.0, [4, 'four'], 5]
 
-x = 7 > 5 # returns a bool - True
-print(f'x is {x}')
 print(type(x))
+print(type(y))
+print(type(x[1]))
+print(id(x[0]))
+print(id(y[0]))
+
+print()
+if x[0] is y[0]:
+  print('Yeah!')
+else:
+  print('Nope!')
+
+print()
+if x is y:
+  print('Yeah!')
+else:
+  print('Nope!')
+
+print()
+if isinstance(y, tuple):
+  print('tuple')
+elif isinstance(y, list):
+  print('list')
+else:
+  print('Nope!')
