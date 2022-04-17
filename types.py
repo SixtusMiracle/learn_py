2b1bfabe5a0032ec69ab4305148d22d9b38e781f
Sixtus Miracle <67114923+SixtusMiracle@users.noreply.github.com>
2022-04-17 12:25:06 +0100

    decimal type

--- types.py
+++ types.py
@@ -1,11 +1,7 @@
-# 7 - int
-# 7 * 3 - int
-# 7.0 - float
-# 7 * 3.14159 - float
-# 7 / 3 - float
-# 7 // 3 - integer division like py2
+from decimal import *
 
-
-x = 7 % 3 # modular operator
+a = Decimal('.10')
+b = Decimal('.30')
+x = a + a + a - b
 print(f'x is {x}')
 print(type(x))
