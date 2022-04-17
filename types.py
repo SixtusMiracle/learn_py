36dd032c1027322cc5a39634b26ff4ec626c87a4
Sixtus Miracle <67114923+SixtusMiracle@users.noreply.github.com>
2022-04-17 12:21:07 +0100

    numeric types

--- types.py
+++ types.py
@@ -1,6 +1,11 @@
-a = 8
-b = 9
+# 7 - int
+# 7 * 3 - int
+# 7.0 - float
+# 7 * 3.14159 - float
+# 7 / 3 - float
+# 7 // 3 - integer division like py2
 
-x = f'seven {a:<09} {b:>09}'
+
+x = 7 % 3 # modular operator
 print(f'x is {x}')
 print(type(x))
