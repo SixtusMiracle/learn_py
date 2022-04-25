bb0f521a8888ae34414e4b45251d3de77a42936a
Sixtus Miracle <67114923+SixtusMiracle@users.noreply.github.com>
2022-04-25 13:08:19 +0100

    additional loop controls

--- loops.py
+++ loops.py
@@ -1,4 +1,15 @@
-animals = ( 'bear', 'bunny', 'dog', 'cat', 'bird' )
+secret = 'swordfish'
+pw = ''
+auth = False
+count = 0
+max_attempt = 5
 
-for pet in range(5):
-  print(pet)
+while pw != secret:
+  count += 1
+  if count > max_attempt: break
+  if count == 3: continue
+  pw = input(f"{count}: What's your secret word?")
+else:
+  auth = True
+
+print("Authorized" if auth else "Calling the FBI ...")
