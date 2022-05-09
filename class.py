df2185abef1f80bd2b98dd297254889dd75205a2
Sixtus Miracle <67114923+SixtusMiracle@users.noreply.github.com>
2022-05-09 16:01:52 +0100

    constructors

--- class.py
+++ class.py
@@ -1,19 +1,30 @@
-class Car:
-    totalDoors = 4
-    totalTires = 4
+class Animal:
+    def __init__(self, **kwargs):
+        self._type = kwargs['type'] if 'type' in kwargs else 'kitten'
+        self._name = kwargs['name'] if 'name' in kwargs else 'fluffy'
+        self._sound = kwargs['sound'] if 'sound' in kwargs else 'rwar'
 
-    def doors(self):
-        print(f'{self.totalDoors} doors')
+    def type(self):
+        return self._type
 
-    def tires(self):
-        print(f'{self.totalTires} tires')
+    def name(self):
+        return self._name
+
+    def sound(self):
+        return self._sound
+
+def print_animal(o):
+    if not isinstance(o, Animal):
+        raise TypeError('print_animal(): requires an Animal')
+    print('The {} is named "{}" and says "{}".'.format(o.type(), o.name(), o.sound()))
 
 
 def main():
-    ferrari = Car()
-    ferrari.doors()
-    ferrari.tires()
+    a0 = Animal(type='kitten', name='fluffy', sound='rwar')
+    a1 = Animal(type='duck', name='donald', sound='quack')
+    print_animal(a0)
+    print_animal(a1)
+    print_animal(Animal(type='velociraptor', name='veronica', sound='hello'))
+    print_animal(Animal())
 
-
-if __name__ == '__main__':
-    main()
+if __name__ == '__main__': main()
