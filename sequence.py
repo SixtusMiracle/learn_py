1ce5b7efc448f78fdb038f33262b3b2b234eefe2
Sixtus Miracle <67114923+SixtusMiracle@users.noreply.github.com>
2022-05-09 15:40:30 +0100

    list comprehension

--- sequence.py
+++ sequence.py
@@ -1,17 +1,17 @@
-# sorted(a) - sort a set
-# a - b members in set a that are not in b
-# a | b members in set a or b
-# a ^ b members in a or b but not both
-# a & b members that are in both
+# seq2 = [x * 2 for x in seq]
+# seq2 = [x for x in seq if x % 3 != 0]
+# seq2 = [(x, x**2) for x in seq]
+# seq2 = [round(pi, i) for i in seq]
+# seq2 = { x: x**2 for x in seq }
 
 def main():
-    a = set("We're gonna need a bigger boat.")
-    b = set("I'm sorry, Dave. I'm afraid I can't do that.")
-    print_set(a | b)
+  seq = range(11)
+  seq2 = { x for x in 'superduper' if x not in 'pd' }
+  print_list(seq)
+  print_list(seq2)
 
-def print_set(o):
-    print('{', end = '')
-    for x in o: print(x, end = '')
-    print('}')
+def print_list(o):
+  for x in o: print(x, end = ' ')
+  print()
 
 if __name__ == '__main__': main()
