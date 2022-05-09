602b50a9f280d547e6bb67e99881ddf18cd150ef
Sixtus Miracle <67114923+SixtusMiracle@users.noreply.github.com>
2022-05-09 12:18:48 +0100

    keyword arguments

--- function.py
+++ function.py
@@ -1,11 +1,11 @@
 def main():
-    soundsList = ('meow', 'meeeh', 'mooh')
-    kitten(*soundsList)
+    x = dict(Buffy = 'meow', Zilla = 'grr', Angel = 'rawr')
+    kitten(**x)
 
-def kitten(*args):
-    if len(args):
-        for sound in args:
-            print(sound)
+def kitten(**kwargs):
+    if len(kwargs):
+        for k in kwargs:
+            print('Kitten {} says {}'.format(k, kwargs[k]))
     else: print('Meow!')
 
 if __name__ == '__main__': main()
