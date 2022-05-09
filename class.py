d196447ed80f2e2c2322edce04a286583ab1c928
Sixtus Miracle <67114923+SixtusMiracle@users.noreply.github.com>
2022-05-09 16:50:35 +0100

    inheritance

--- class.py
+++ class.py
@@ -1,28 +1,49 @@
 class Animal:
     def __init__(self, **kwargs):
-        self._type = kwargs['type'] if 'type' in kwargs else 'kitten'
-        self._name = kwargs['name'] if 'name' in kwargs else 'fluffy'
-        self._sound = kwargs['sound'] if 'sound' in kwargs else 'meow'
+        if 'type' in kwargs: self._type = kwargs['type']
+        if 'name' in kwargs: self._name = kwargs['name']
+        if 'sound' in kwargs: self._sound = kwargs['sound']
 
     def type(self, t = None):
         if t: self._type = t
-        return self._type
+        try: return self._type
+        except AttributeError: return None
 
     def name(self, n = None):
         if n: self._name = n
-        return self._name
+        try: return self._name
+        except AttributeError: return None
 
     def sound(self, s = None):
         if s: self._sound = s
-        return self._sound
+        try: return self._sound
+        except AttributeError: return None
 
-    def __str__(self):
-        return f'The {self.type()} is named "{self.name()}" and says "{self.sound()}".'
+class Duck(Animal):
+    def __init__(self, **kwargs):
+        self._type = 'duck'
+        if 'type' in kwargs: del kwargs['type']
+        super().__init__(**kwargs)
+
+class Kitten(Animal):
+    def __init__(self, **kwargs):
+        self._type = 'kitten'
+        if 'type' in kwargs: del kwargs['type']
+        super().__init__(**kwargs)
+    
+    def kill(self, specie):
+        print(f'{self.name()} will now kill all {specie}')
+
+def print_animal(o):
+    if not isinstance(o, Animal):
+        raise TypeError('print_animal(): requires an Animal')
+    print(f'The {o.type()} is named "{o.name()}" and says "{o.sound()}".')
 
 def main():
-    a0 = Animal(type = 'kitten', name = 'fluffy', sound = 'rwar')
-    a1 = Animal(type = 'duck', name = 'donald', sound = 'quack')
-    print(a0)
-    print(a1)
+    a0 = Kitten(name = 'fluffy', sound = 'rwar')
+    a1 = Duck(name = 'donald', sound = 'quack')
+    print_animal(a0)
+    print_animal(a1)
+    a0.kill('humans')
 
 if __name__ == '__main__': main()
