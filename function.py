26b1b585ba6f337f202f96313c494a8ed528a3c7
Sixtus Miracle <67114923+SixtusMiracle@users.noreply.github.com>
2022-05-09 12:34:18 +0100

    decorators

--- function.py
+++ function.py
@@ -1,28 +1,21 @@
+import time
+
+def elapsed_time(f):
+    def wrapper():
+        t1 = time.time()
+        f()
+        t2 = time.time()
+        print(f'Elapsed time: {(t2 - t1) * 1000} ms')
+    return wrapper
+
+@elapsed_time
+def big_sum():
+    num_list = []
+    for num in (range(0, 10000)):
+        num_list.append(num)
+    print(f'Big sum: {sum(num_list)}')
+
 def main():
-    for i in inclusive_range(25):
-        print(i, end = ' ')
-    print()
-
-def inclusive_range(*args):
-    numargs = len(args)
-    start = 0
-    step = 1
-
-    # initialize parameters
-    if numargs < 1:
-        raise TypeError(f'expected at least 1 argument, got {numargs}')
-    elif numargs == 1:
-        stop = args[0]
-    elif numargs == 2:
-        (start, stop) = args
-    elif numargs == 3:
-        (start, stop, step) = args
-    else: raise TypeError(f'expected at most 3 arguments, got {numargs}')
-
-    # generator
-    i = start
-    while i <= stop:
-        yield i
-        i += step
+    big_sum()
 
 if __name__ == '__main__': main()
