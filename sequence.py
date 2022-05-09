9221d754631b921be998340031152041237cc8e5
Sixtus Miracle <67114923+SixtusMiracle@users.noreply.github.com>
2022-05-09 13:37:20 +0100

    manipulating lists

--- sequence.py
+++ sequence.py
@@ -1,6 +1,20 @@
-x = { 'one': 1, 'two': 2, 'three': 3, 'four': 4, 'five': 5}
+# game.index('Paper') - search through a list, returns the index
+# game.print(game[1:5:2]) - slice of a list, works like range
+# game.append('Computer') - add an item at the end of a list
+# game.insert(0, 'Music') - add an item at a particular index
+# game.remove('Paper') - remove an item
+# game.pop(3) - returns the removed item
+# del game[3] - removes an item by index or by slice
+# print(', '.join(game)) - join to a list using string function join()
+# print(len(game)) - length of a list
 
-x['three'] = 39
+def main():
+  game = [ 'Rock', 'Paper', 'Scissors','Lizard', 'Spock' ]
+  print(len(game))
+  print_list(game)
 
-for k, v in x.items():
-  print(f'k: {k}, v: {v}')
+def print_list(o):
+  for i in o: print(i, end = ' ', flush = True)
+  print()
+
+if __name__ == '__main__': main()
