6054175f44ff4de3c084d486cb6e5c192d28c61b
Sixtus Miracle <67114923+SixtusMiracle@users.noreply.github.com>
2022-05-09 12:19:34 +0100

    generators

--- function.py
+++ function.py
@@ -1,11 +1,28 @@
 def main():
-    x = dict(Buffy = 'meow', Zilla = 'grr', Angel = 'rawr')
-    kitten(**x)
+    for i in inclusive_range(25):
+        print(i, end = ' ')
+    print()
 
-def kitten(**kwargs):
-    if len(kwargs):
-        for k in kwargs:
-            print('Kitten {} says {}'.format(k, kwargs[k]))
-    else: print('Meow!')
+def inclusive_range(*args):
+    numargs = len(args)
+    start = 0
+    step = 1
+
+    # initialize parameters
+    if numargs < 1:
+        raise TypeError(f'expected at least 1 argument, got {numargs}')
+    elif numargs == 1:
+        stop = args[0]
+    elif numargs == 2:
+        (start, stop) = args
+    elif numargs == 3:
+        (start, stop, step) = args
+    else: raise TypeError(f'expected at most 3 arguments, got {numargs}')
+
+    # generator
+    i = start
+    while i <= stop:
+        yield i
+        i += step
 
 if __name__ == '__main__': main()
